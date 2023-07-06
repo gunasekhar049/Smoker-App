4eea653cc20922259cacedbbd7320e97a1af5baa
Sompalle.sekhar@synoriq.in <GunA@049>
2023-07-06 16:42:36 +0530

    created a app and added gif and touch the screen the gif will works

--- app/src/main/java/com/example/smokerapp/MainActivity.java
+++ app/src/main/java/com/example/smokerapp/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.smokerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.bumptech.glide.Glide;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GifImageView gifImageView;
+    private GifDrawable gifDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gifImageView = findViewById(R.id.gifImageView);
+        gifDrawable = (GifDrawable) gifImageView.getDrawable();
+        gifDrawable.pause();
+
+        // Add a listener to detect exhaling
+        gifImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                if (action == MotionEvent.ACTION_DOWN) {
+                    // Exhale detected, resume the GIF
+                    gifDrawable.start();
+                } else if (action == MotionEvent.ACTION_UP) {
+                    // Inhale detected, pause the GIF
+                    gifDrawable.pause();
+                }
+                return true;
+            }
+        });
     }
 }
